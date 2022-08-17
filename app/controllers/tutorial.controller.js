81a78ef1623175299f4ee5c624ccb0dafda651d0
Austin Thome <austin.thome1@gmail.com>
2022-08-16 22:05:49 -0500

    initial commit

--- app/controllers/tutorial.controller.js
+++ app/controllers/tutorial.controller.js
@@ -1,34 +1,8 @@
 const db = require("../models");
 const Tutorial = db.tutorials;
 const Op = db.Sequelize.Op;
+
 // Create and Save a new Tutorial
-exports.create = (req, res) => {
-
-};
-// Retrieve all Tutorials from the database.
-exports.findAll = (req, res) => {
-
-};
-// Find a single Tutorial with an id
-exports.findOne = (req, res) => {
-
-};
-// Update a Tutorial by the id in the request
-exports.update = (req, res) => {
-
-};
-// Delete a Tutorial with the specified id in the request
-exports.delete = (req, res) => {
-
-};
-// Delete all Tutorials from the database.
-exports.deleteAll = (req, res) => {
-
-};
-// Find all published Tutorials
-exports.findAllPublished = (req, res) => {
-
-};
 exports.create = (req, res) => {
   // Validate request
   if (!req.body.title) {
@@ -37,12 +11,14 @@ exports.create = (req, res) => {
     });
     return;
   }
+
   // Create a Tutorial
   const tutorial = {
     title: req.body.title,
     description: req.body.description,
     published: req.body.published ? req.body.published : false
   };
+
   // Save Tutorial in the database
   Tutorial.create(tutorial)
     .then(data => {
@@ -55,8 +31,49 @@ exports.create = (req, res) => {
       });
     });
 };
+
+// Retrieve all Tutorials from the database.
+exports.findAll = (req, res) => {
+  const title = req.query.title;
+  var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+
+  Tutorial.findAll({ where: condition })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials."
+      });
+    });
+};
+
+// Find a single Tutorial with an id
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  Tutorial.findByPk(id)
+    .then(data => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find Tutorial with id=${id}.`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving Tutorial with id=" + id
+      });
+    });
+};
+
+// Update a Tutorial by the id in the request
 exports.update = (req, res) => {
   const id = req.params.id;
+
   Tutorial.update(req.body, {
     where: { id: id }
   })
@@ -77,8 +94,11 @@ exports.update = (req, res) => {
       });
     });
 };
+
+// Delete a Tutorial with the specified id in the request
 exports.delete = (req, res) => {
   const id = req.params.id;
+
   Tutorial.destroy({
     where: { id: id }
   })
@@ -99,6 +119,8 @@ exports.delete = (req, res) => {
       });
     });
 };
+
+// Delete all Tutorials from the database.
 exports.deleteAll = (req, res) => {
   Tutorial.destroy({
     where: {},
@@ -114,6 +136,8 @@ exports.deleteAll = (req, res) => {
       });
     });
 };
+
+// find all published Tutorial
 exports.findAllPublished = (req, res) => {
   Tutorial.findAll({ where: { published: true } })
     .then(data => {
--- server.js
+++ server.js
@@ -1,26 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+
 const app = express();
+
 var corsOptions = {
   origin: "http://localhost:8081"
 };
+
 app.use(cors(corsOptions));
+
 // parse requests of content-type - application/json
 app.use(express.json());
+
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
-});
-require("./app/routes/turorial.routes")(app);
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
-// call sync method
+
 const db = require("./app/models");
+
 db.sequelize.sync()
   .then(() => {
     console.log("Synced db.");
@@ -28,6 +24,21 @@ db.sequelize.sync()
   .catch((err) => {
     console.log("Failed to sync db: " + err.message);
   });
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and re-sync db.");
+
+// // drop the table if it already exists
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+require("./app/routes/tutorial.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
